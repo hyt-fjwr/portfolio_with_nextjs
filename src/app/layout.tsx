dbfeb3b335d591a22722c2bf397521d604c5a36f
crow-editing <f.hayato0104@gmail.com>
2023-06-13 00:33:55 +0900

    Changed landing page design. Added page transition.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,25 @@ export default function RootLayout({
         {/* <Navbar /> */}
         {/* Landing Page */}
         <div className='w-full overflow-hidden'>
-          <h1 className='absolute z-10 top-1/2 translate-y-1/2 translate-x-1/2 text-4xl'>HAYATO FUJIWARA</h1>
+          <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-zinc-50'>
+            <h1 className='text-4xl font-bold tracking-wide'>HAYATO FUJIWARA</h1>
+            <h3 className='font-Roboto font-medium tracking-wide text-sm'>Web Developer</h3>
+            <h3 className='font-Roboto font-medium tracking-wide text-sm'>Designer</h3>
+            <h3 className='font-Roboto font-medium tracking-wide text-sm'>Editor</h3>
+          </div>
           <div className={styles.lpbg}></div>
+
+          <div className='absolute top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'>
+            <div className='flex justify-center'>
+              <div className='rounded-full w-14 h-14 border border-x-zinc-50'>
+                <a href="#" className='relative flex justify-center top-1/4 hover:translate-y-1 duration-300'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="white" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
         {children}
         <footer></footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Heebo: ['Heebo'],
+        Oswald: ['Oswald'],
+        Poppins: ['Poppins'],
+        Roboto: ['Roboto'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        "slide-in-bottom": {
+          "0%": {
+            transform: "translateY(20px)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
       },
     },
   },
